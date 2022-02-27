17177ccd27d8bb50dc2eff746932156ec18e1626
Alain D'Ettorre <alain.det@gmail.com>
2022-02-27 17:14:26 +0000

    Add some slices examples

--- courses/pro-go/collections/main.go
+++ courses/pro-go/collections/main.go
@@ -45,6 +45,56 @@ func arraysExamples() {
 	// #2 => Backpack
 }
 
+func slicesExamples() {
+	// This is inefficient, since "append()" needs to allocate a new bigger array,
+	// copy old values and then append new ones
+	// names := []string{"Kayak", "Lifejacket", "Paddle"}
+	// names = append(names, "Hat", "Gloves")
+	// fmt.Println(names)
+
+	// This creates a new slice with length 3 and capacity 6
+	// Capacity is the backing array's length
+	names := make([]string, 3, 6)
+	names[0] = "Kayak"
+	names[1] = "Lifejacket"
+	names[2] = "Paddle"
+	fmt.Println("len:", len(names), ", cap:", cap(names)) // len: 3, cap: 6
+
+	aList := [6]string{
+		"Foo",
+		"Bar",
+		"Baz",
+		"Tic",
+		"Tac",
+		"Toe",
+	}
+
+	slice1 := aList[:]
+	slice2 := aList[2:3]
+
+	// []string, []string{"Foo", "Bar", "Baz", "Tic", "Tac", "Toe"}
+	fmt.Printf("%T, %#v\n", slice1, slice1)
+
+	// []string, []string{"Baz"}
+	fmt.Printf("%T, %#v\n", slice2, slice2)
+}
+
+func slicesExamples2() {
+	products := [4]string{"Kayak", "Lifejacket", "Paddle", "Hat"}
+	someNames := products[1:3]
+
+	// [Lifejacket Paddle], len:2, cap:3
+	fmt.Printf("%v, len:%d, cap:%d\n", someNames, len(someNames), cap(someNames))
+
+	someNames = append(someNames, "Gloves")
+
+	// WARNING: someNames did "expand"
+	// [Lifejacket Paddle Gloves], len:3, cap:3
+	fmt.Printf("%v, len:%d, cap:%d\n", someNames, len(someNames), cap(someNames))
+}
+
 func main() {
-	arraysExamples()
+	// arraysExamples()
+	// slicesExamples()
+	slicesExamples2()
 }
