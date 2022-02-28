45b8ed73297a3dfe0006e8b3545e4caeafc0c4c9
Alain D'Ettorre <alain.det@gmail.com>
2022-02-28 09:06:18 +0100

    Add examples about slices

--- courses/pro-go/collections/main.go
+++ courses/pro-go/collections/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"reflect"
+	"sort"
 )
 
 func arraysExamples() {
@@ -93,8 +95,90 @@ func slicesExamples2() {
 	fmt.Printf("%v, len:%d, cap:%d\n", someNames, len(someNames), cap(someNames))
 }
 
+/**
+ * Copying with ranges!
+ */
+func slicesExamples3() {
+	products := [4]string{"Kayak", "Lifejacket", "Paddle", "Hat"}
+	allNames := products[1:]
+	someNames := []string{"Boots", "Canoe"}
+	fmt.Println("someNames[1:]:", someNames[1:]) // someNames[1:]: [Canoe]
+	fmt.Println("allNames[2:3]:", allNames[2:3]) // allNames[2:3]: [Hat]
+	copy(someNames[1:], allNames[2:3])
+	fmt.Println("someNames:", someNames) // someNames: [Boots Hat]
+	fmt.Println("allNames", allNames)    // allNames [Lifejacket Paddle Hat]
+
+	// With smaller source
+	dest1 := []string{"aaa", "bbb", "ccc", "ddd"}
+	src1 := []string{"eee", "fff"}
+	copy(dest1, src1)
+	fmt.Println(dest1) // [eee fff ccc ddd]
+
+	// With smaller destination
+	dest2 := []string{"aaa", "bbb", "ccc", "ddd"}
+	src2 := []string{"eee", "fff", "ggg"}
+	copy(dest2[0:2], src2)
+	fmt.Println(dest2) // [eee fff ccc ddd]
+}
+
+/**
+ * To delete a value from an array, just glue the two parts together and omit
+ * the deleted part
+ */
+func slicesExamples4() {
+	arr := [4]string{"aa", "bb", "cc", "dd"}
+	i := 2
+	deleted := append(arr[:i], arr[i+1:]...)
+	fmt.Println(deleted) // [aa bb dd]
+}
+
+/**
+ * Sorting is performed via packages
+ */
+func slicesExamples5() {
+	letters := []string{"cc", "bb", "aa", "dd"}
+	sort.Strings(letters)
+	fmt.Println(letters) // [aa bb cc dd]
+
+	nums := []int{4, 6, 3, 7, 1}
+	sort.Ints(nums)
+	fmt.Println(nums) // [1 3 4 6 7]
+}
+
+/**
+ * Comparing slices is not directly possible!
+ */
+func slicesExamples6() {
+	p1 := []string{"Kayak", "Lifejacket", "Paddle", "Hat"}
+	p2 := p1
+	p3 := p1[1:]
+	p4 := p1[:]
+	compareP1AndP2 := reflect.DeepEqual(p1, p2)
+	compareP1AndP3 := reflect.DeepEqual(p1, p3)
+	compareP1AndP4 := reflect.DeepEqual(p1, p4)
+	fmt.Println(compareP1AndP2, compareP1AndP3, compareP1AndP4) // true false true
+}
+
+/**
+ * Access the underlying array
+ */
+func slicesExamples7() {
+	p1 := []string{"aa", "bb", "cc", "dd"}
+	arrayPtr := (*[3]string)(p1)
+	array := *arrayPtr // <-- This is a copy of the underlying array of the slice p1
+	fmt.Println(array) // [aa bb cc]
+	array[0] = "zz"
+	fmt.Println(array) // [zz bb cc]
+	fmt.Println(p1)    // [aa bb cc dd]
+}
+
 func main() {
 	// arraysExamples()
 	// slicesExamples()
-	slicesExamples2()
+	// slicesExamples2()
+	// slicesExamples3()
+	// slicesExamples4()
+	// slicesExamples5()
+	// slicesExamples6()
+	slicesExamples7()
 }
