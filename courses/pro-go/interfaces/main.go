6ce92219be262dcd9509e9149a7c056439007310
Alain D'Ettorre <alain.det@gmail.com>
2022-03-03 11:39:44 +0100

    Add examples for the empty interface

--- courses/pro-go/interfaces/main.go
+++ courses/pro-go/interfaces/main.go
@@ -33,22 +33,56 @@ func calcTotalExpense(expenses []Expense) (total float64) {
 	return
 }
 
-func main() {
-	expenses := []Expense{
-		Service{"Boat Cover", 12, 89.50, []string{}},
-		Service{"Paddle Protect", 12, 8, []string{}},
-		&Product{"Kayak", "Watersports", 275},
-	}
+type Person struct {
+	name, city string
+}
 
-	for _, expense := range expenses {
-
-		// If it's a service, use Service-specific fields
-		if s, ok := expense.(Service); ok {
-			fmt.Println("Service:", s.description, "Price:",
-				s.monthlyFee*float64(s.durationMonths))
-		} else { // Else, use the interface method
-			fmt.Println("Expense:", expense.getName(),
-				"Cost:", expense.getCost(true))
+func processItems(items ...interface{}) {
+	for _, item := range items {
+		switch value := item.(type) {
+		case Product:
+			fmt.Printf("Product: %s, Price: %.2f\n", value.name, value.price)
+		case *Product:
+			fmt.Printf("Product Pointer: %s, Price: %.2f\n", value.name, value.price)
+		case Service:
+			fmt.Printf(
+				"Service: %s, Price: %.2f\n",
+				value.description,
+				value.monthlyFee*float64(value.durationMonths),
+			)
+		case Person:
+			fmt.Printf("Person: %s, City: %s\n", value.name, value.city)
+		case *Person:
+			fmt.Printf("Person Pointer: %s, City: %s\n", value.name, value.city)
+		case string, bool, int:
+			fmt.Printf("Built-in type: %v\n", value)
+		default:
+			fmt.Printf("Default: %v\n", value)
 		}
 	}
+}
+
+func main() {
+	var expense Expense = &Product{"Kayak", "Watersports", 275}
+
+	data := []interface{}{
+		expense,
+		Product{"Lifejacket", "Watersports", 48.95},
+		Service{"Boat Cover", 12, 89.50, []string{}},
+		Person{"Alice", "London"},
+		&Person{"Bob", "New York"},
+		"This is a string",
+		100,
+		true,
+	}
+
+	processItems(data...)
+	// Product Pointer: Kayak, Price: 275.00
+	// Product: Lifejacket, Price: 48.95
+	// Service: Boat Cover, Price: 1074.00
+	// Person: Alice, City: London
+	// Person Pointer: Bob, City: New York
+	// Built-in type: This is a string
+	// Built-in type: 100
+	// Built-in type: true
 }
