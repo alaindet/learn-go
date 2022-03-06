52bfb486906488f93def249ba8402a82784eedff
Alain D'Ettorre <alain.det@gmail.com>
2022-03-06 18:01:22 +0100

    Add examples on errors

--- courses/pro-go/_notes/errors.md
+++ courses/pro-go/_notes/errors.md
@@ -6,5 +6,14 @@
       Error() string
   }
   ```
-- A `panic` function is used when generating a fatal blocking error
 - The `errors` standard package has functions to create and manage errors
+
+## Panicking
+- A `panic()` function is used when generating a fatal blocking error
+- When a panic error is generated, it triggers all the `defer` statements in the current function, then bubbles up the stack interrupting the execution
+- Whether a function should trigger a normal error or a panic error should be decided by the calling function which has context
+- Conventionally, such functions should have two version, one of which panics and is prefix with `Must` (Ex.: the `regexp` standard package has `Compile` and `MustCompile` functions)
+- The built-in `recover()` function must be called via `defer` and can capture a panic error
+  ```go
+
+  ```
--- courses/pro-go/error-handling/main.go
+++ courses/pro-go/error-handling/main.go
@@ -2,20 +2,54 @@ package main
 
 import "fmt"
 
-func main() {
+type CategoryCountMessage struct {
+	Category      string
+	Count         int
+	TerminalError interface{}
+}
 
-	categories := []string{"Watersports", "Chess", "Running"}
+func processCategories(
+	categories []string,
+	outChan chan<- CategoryCountMessage,
+) {
+	defer func() {
+		if arg := recover(); arg != nil {
+			fmt.Println("Panic recovered", arg)
+			outChan <- CategoryCountMessage{
+				TerminalError: arg,
+			}
+			close(outChan)
+		}
+	}()
+
 	channel := make(chan ChannelMessage, 10)
-
-	// Calculate total prices
 	go Products.TotalPriceAsync(categories, channel)
 
-	// Listen for messsages
 	for message := range channel {
 		if message.CategoryError != nil {
-			fmt.Println(message.CategoryError.Error())
+			panic(message.CategoryError)
+		}
+
+		outChan <- CategoryCountMessage{
+			Category: message.Category,
+			Count:    int(message.Total),
+		}
+	}
+
+	close(outChan)
+}
+
+func main() {
+	categories := []string{"Watersports", "Chess", "Running"}
+	channel := make(chan CategoryCountMessage)
+
+	go processCategories(categories, channel)
+
+	for message := range channel {
+		if message.TerminalError == nil {
+			fmt.Println(message.Category, "Total:", message.Count)
 		} else {
-			fmt.Println(message.Category, "Total:", ToCurrency(message.Total))
+			fmt.Println("ERROR: A terminal error occured")
 		}
 	}
 }
