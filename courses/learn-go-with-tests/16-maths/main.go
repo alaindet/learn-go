3fd5fc6e17c8a0e32c22285e3e8922be62233e91
Alain D'Ettorre <alain.det@gmail.com>
2022-04-30 12:26:30 +0200

    WIP: Add boilerplate for math-time problem

--- courses/learn-go-with-tests/16-maths/main.go
+++ courses/learn-go-with-tests/16-maths/main.go
@@ -1,1 +1,12 @@
 package main
+
+import "time"
+
+type Point struct {
+	X float64
+	Y float64
+}
+
+func SecondHand(t time.Time) Point {
+	return Point{}
+}
--- courses/learn-go-with-tests/16-maths/main_test.go
+++ courses/learn-go-with-tests/16-maths/main_test.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"testing"
+	"time"
+)
+
+func TestSecondHandAtMidnight(t *testing.T) {
+	input := time.Date(1970, time.January, 1, 0, 0, 0, 0, time.UTC)
+
+	expected := Point{X: 150, Y: 150 - 90}
+	result := SecondHand(input)
+
+	if result != expected {
+		t.Errorf("result: %v expected: %v", result, expected)
+	}
+}
