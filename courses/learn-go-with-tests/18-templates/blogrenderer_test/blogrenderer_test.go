14d138ca12527218e4ab8bc7b4f10b9b803342b0
Alain D'Ettorre <alain.det@gmail.com>
2022-05-05 00:58:52 +0200

    WIP: Add first approval test

--- courses/learn-go-with-tests/18-templates/blogrenderer_test/blogrenderer_test.go
+++ courses/learn-go-with-tests/18-templates/blogrenderer_test/blogrenderer_test.go
@@ -2,9 +2,11 @@ package blogrenderer_test
 
 import (
 	"bytes"
-	"learn_go_with_tests/templates/blogrenderer"
-	"strings"
 	"testing"
+
+	approvals "github.com/approvals/go-approval-tests"
+
+	"learn_go_with_tests/templates/blogrenderer"
 )
 
 func TestRender(t *testing.T) {
@@ -24,25 +26,36 @@ func TestRender(t *testing.T) {
 			t.Fatal(err)
 		}
 
-		result := buf.String()
-
-		// TODO: Move to approval tests
-		expected := joinStrings(
-			"<h1>hello world</h1>",
-			"<p>This is a description</p>",
-			"Tags: <ul><li>go</li><li>tdd</li></ul>",
-		)
-
-		if result != expected {
-			t.Errorf("got '%s' want '%s'", result, expected)
-		}
+		approvals.VerifyString(t, buf.String())
 	})
+
+	// t.Run("it converts a single post into HTML", func(t *testing.T) {
+	// 	buf := bytes.Buffer{}
+	// 	err := blogrenderer.Render(&buf, aPost)
+
+	// 	if err != nil {
+	// 		t.Fatal(err)
+	// 	}
+
+	// 	result := buf.String()
+
+	// 	// TODO: Move to approval tests
+	// 	expected := joinStrings(
+	// 		"<h1>hello world</h1>",
+	// 		"<p>This is a description</p>",
+	// 		"Tags: <ul><li>go</li><li>tdd</li></ul>",
+	// 	)
+
+	// 	if result != expected {
+	// 		t.Errorf("got '%s' want '%s'", result, expected)
+	// 	}
+	// })
 }
 
 // func joinLines(lines ...string) string {
 // 	return strings.Join(lines, "\n")
 // }
 
-func joinStrings(lines ...string) string {
-	return strings.Join(lines, "")
-}
+// func joinStrings(lines ...string) string {
+// 	return strings.Join(lines, "")
+// }
--- courses/learn-go-with-tests/18-templates/go.mod
+++ courses/learn-go-with-tests/18-templates/go.mod
@@ -1,3 +1,5 @@
 module learn_go_with_tests/templates
 
 go 1.18
+
+require github.com/approvals/go-approval-tests v0.0.0-20220429035603-e60d9ca44d3e // indirect
