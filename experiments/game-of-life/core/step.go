f74e7d4748d27e65c5bc2e1e581bdede5f53d54c
Alain D'Ettorre <alain.det@gmail.com>
2022-05-04 02:05:56 +0200

    Refactor Game of Life core package

--- experiments/game-of-life/core/step.go
+++ experiments/game-of-life/core/step.go
@@ -1,21 +1,17 @@
 package core
 
 func (g *Game) Step() {
-	size := len(g.State)
-	newState := make([]bool, size)
+	g.Generation++
+	newState := make([]bool, len(g.State))
 
-	for i := 0; i < size; i++ {
-		neighbors := g.CountNeighbors(i)
+	for i := 0; i < len(g.State); i++ {
 		newState[i] = false // Cells die unless specific conditions arise
-		isAlive := g.State[i]
-		shouldSurvive := isAlive && (neighbors == 2 || neighbors == 3)
-		shouldBeBorn := !isAlive && neighbors == 3
-		if shouldSurvive || shouldBeBorn {
+		n := g.CountNeighbors(i)
+		if n == 3 || (n == 2 && g.State[i]) {
 			newState[i] = true
 		}
 	}
 
-	g.Generation++
 	g.State = newState
 }
 
@@ -27,22 +23,21 @@ func (g *Game) Steps(steps int) {
 
 func (g *Game) CountNeighbors(i int) int {
 	neighbors := 0
-	size := len(g.State)
 	lookArounds := []int{
-		i - g.Width,
-		i - g.Width + 1,
-		i + 1,
-		i + g.Width + 1,
-		i + g.Width,
-		i + g.Width - 1,
-		i - 1,
-		i - g.Width - 1,
+		i - g.Width - 1, // top-left
+		i - g.Width,     // top
+		i - g.Width + 1, // top-right
+		i - 1,           // left
+		i + 1,           // right
+		i + g.Width - 1, // bottom-left
+		i + g.Width,     // bottom
+		i + g.Width + 1, // bottom-right
 	}
 
 	for _, lookAround := range lookArounds {
 
 		// No neighbor in this direction, it's considered dead
-		if lookAround < 0 || lookAround >= size {
+		if lookAround < 0 || lookAround >= len(g.State) {
 			continue
 		}
 
