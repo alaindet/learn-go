17ef9b990c356b4a6d7effffd66da0c675b7ba91
Alain D'Ettorre <alain.det@gmail.com>
2022-06-08 15:15:51 +0200

    Add Makefile

--- courses/working-with-concurrency/final-project/cmd/web/main.go
+++ courses/working-with-concurrency/final-project/cmd/web/main.go
@@ -1,7 +1,80 @@
 package main
 
-const webPort = "80"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+const (
+	dbMaxTries = 10
+	dbRetryIn  = 1000 // milliseconds
+	webPort    = "80"
+)
+
+var db *sql.DB
 
 func main() {
+	db := initDB()
+	fmt.Println("db", db)
+}
 
+// TODO: Singleton with sync.Once()?
+func initDB() *sql.DB {
+	if db == nil {
+		conn, err := connectToDB()
+
+		if err != nil {
+			log.Panic("cannot connect to database")
+		}
+
+		db = conn
+	}
+
+	return db
+}
+
+func connectToDB() (*sql.DB, error) {
+	tries := 0
+	dns := os.Getenv("DSN")
+
+	for {
+		conn, err := openDB(dns)
+
+		if tries > dbMaxTries {
+			return nil, fmt.Errorf("cannot connect to the database")
+		}
+
+		if err != nil {
+			log.Println("database not ready yet")
+			tries++
+			time.Sleep(time.Duration(dbRetryIn) * time.Millisecond)
+			continue
+		}
+
+		log.Println("connected to database")
+		return conn, nil
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
