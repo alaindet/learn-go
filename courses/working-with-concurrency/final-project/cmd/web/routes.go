e6cd318e37ab20b737d614b54b7fc2e0bd2d6006
Alain D'Ettorre <alain.det@gmail.com>
2022-06-10 07:06:23 +0200

    Add stubs for http handlers

--- courses/working-with-concurrency/final-project/cmd/web/routes.go
+++ courses/working-with-concurrency/final-project/cmd/web/routes.go
@@ -17,6 +17,12 @@ func (app *Config) routes() http.Handler {
 
 	// Routes
 	mux.Get("/", app.HomePage)
+	mux.Get("/login", app.LoginPage)
+	mux.Post("/login", app.PostLoginPage)
+	mux.Get("/logout", app.Logout)
+	mux.Get("/register", app.RegisterPage)
+	mux.Post("/register", app.PostRegisterPage)
+	mux.Post("/activate", app.ActivateAccount)
 	// Add global routes here...
 
 	return mux
