5a2b550524b784e6839eb0663d566fa557935dbd
Alain D'Ettorre <alain.det@gmail.com>
2022-06-14 18:43:35 +0200

    WIP

--- courses/lets-go/snippetbox/main.go
+++ courses/lets-go/snippetbox/main.go
@@ -11,6 +11,9 @@ func main() {
 
 	// Handlers
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	// Bootstrap
 	log.Println("Starting server on :8080")
@@ -20,4 +23,12 @@ func main() {
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
 }
