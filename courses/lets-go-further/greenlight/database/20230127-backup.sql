b30da140a649ba99132134d1f7555f769e9a969b
Alain D'Ettorre <alain.det@gmail.com>
2023-01-27 08:14:32 +0100

    Add SQL backup

---
+++ courses/lets-go-further/greenlight/database/20230127-backup.sql
@@ -0,0 +1,47 @@
+INSERT INTO
+  "movies" (
+    "id",
+    "created_at",
+    "title",
+    "year",
+    "runtime",
+    "genres",
+    "version"
+  )
+VALUES
+  (
+    1,
+    '2023-01-27 07:02:17+00',
+    'The Matrix',
+    1999,
+    136,
+    '{sci-fi,action}',
+    1
+  ),
+  (
+    2,
+    '2023-01-27 07:03:26+00',
+    'Deadpool',
+    2016,
+    108,
+    '{action,comedy}',
+    1
+  ),
+  (
+    3,
+    '2023-01-27 07:03:35+00',
+    'The Breakfast Club',
+    1986,
+    96,
+    '{drama}',
+    1
+  ),
+  (
+    4,
+    '2023-01-27 07:04:21+00',
+    'Catch Me If You Can',
+    2002,
+    141,
+    '{biography,comedy,drama}',
+    1
+  );
