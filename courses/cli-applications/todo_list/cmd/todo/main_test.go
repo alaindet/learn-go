e43b98807c96aa0703ad641365df90218f4836f1
Alain D'Ettorre <alain.det@gmail.com>
2023-02-14 13:42:13 +0100

    Add integration test for todo list app

--- courses/cli-applications/todo_list/cmd/todo/main_test.go
+++ courses/cli-applications/todo_list/cmd/todo/main_test.go
@@ -1,1 +1,71 @@
-package main
+package main_test
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"testing"
+)
+
+var (
+	binName  = "todo"
+	fileName = "todos.json"
+)
+
+func TestMain(m *testing.M) {
+
+	fmt.Println("Building tool...")
+
+	if runtime.GOOS == "windows" {
+		binName += ".exe"
+	}
+
+	build := exec.Command("go", "build", "-o", binName, "./cmd/todo")
+	err := build.Run()
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Cannot build tool %s: %s", binName, err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Running tests....")
+	result := m.Run()
+	fmt.Println("Cleaning up...")
+	os.Remove(binName)
+	os.Remove(fileName)
+	os.Exit(result)
+}
+
+func TestTodoCLI(t *testing.T) {
+	todo := "test todo number 1"
+	dir, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+	cmdPath := filepath.Join(dir, binName)
+
+	t.Run("AddNewTodo", func(t *testing.T) {
+		cmd := exec.Command(cmdPath, strings.Split(todo, " ")...)
+		err := cmd.Run()
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("ListTodos", func(t *testing.T) {
+		cmd := exec.Command(cmdPath)
+		stdout, stderr := cmd.CombinedOutput()
+		if stderr != nil {
+			t.Fatal(err)
+		}
+		result := string(stdout)
+		expected := todo + "\n"
+
+		if expected != result {
+			t.Errorf("Expected %q, got %q instead\n", expected, result)
+		}
+	})
+}
