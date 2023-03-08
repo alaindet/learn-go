eaada140ab249664487b749f8f7f3b5caf098061
Alain D'Ettorre <alain.det@gmail.com>
2023-03-08 08:46:47 +0100

    Add helper to setup git fake remotes for intergration testing

--- courses/cli-applications/go_ci/run_test.go
+++ courses/cli-applications/go_ci/run_test.go
@@ -3,38 +3,58 @@ package main
 import (
 	"bytes"
 	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"testing"
 )
 
 func TestRun(t *testing.T) {
+
+	// Skip all if not git command is found
+	_, err := exec.LookPath("git")
+	if err != nil {
+		t.Skip("Git not installed. Skipping test.")
+	}
+
 	var testCases = []struct {
 		name           string
 		inputProject   string
 		expectedOutput string
 		expectedError  error
+		setupGit       bool
 	}{
 		{
 			name:           "success",
 			inputProject:   "./testdata/tool/",
 			expectedOutput: "Go Build: SUCCESS\nGo Test: SUCCESS\nGofmt: SUCCESS\nGit Push: SUCCESS\n",
 			expectedError:  nil,
+			setupGit:       true,
 		},
 		{
 			name:           "fail",
 			inputProject:   "./testdata/tool_err",
 			expectedOutput: "",
 			expectedError:  &stepErr{step: "go build"},
+			setupGit:       false,
 		},
 		{
 			name:           "failformat",
 			inputProject:   "./testdata/tool_gofmt_err",
 			expectedOutput: "",
 			expectedError:  &stepErr{step: "go fmt"},
+			setupGit:       false,
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
+
+			if tc.setupGit {
+				cleanup := setupGit(t, tc.inputProject)
+				defer cleanup()
+			}
 
 			var out bytes.Buffer
 
@@ -62,5 +82,69 @@ func TestRun(t *testing.T) {
 				t.Errorf("Expected output: %q. Got %q", tc.expectedOutput, out.String())
 			}
 		})
+	}
+}
+
+// Test helper
+func setupGit(t *testing.T, projectDir string) func() {
+	t.Helper()
+
+	// Search for installed git command
+	gitExec, err := exec.LookPath("git")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a temporary directory
+	tempDir, err := os.MkdirTemp("", "gocitest")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Make project path absolute if not already absolute
+	projectPath, err := filepath.Abs(projectDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a fake remote Git origin
+	remoteURI := fmt.Sprintf("file://%s", tempDir)
+
+	var gitCommands = []struct {
+		args []string
+		dir  string
+		env  []string
+	}{
+		{[]string{"init", "--bare"}, tempDir, nil},
+		{[]string{"init"}, projectDir, nil},
+		{[]string{"remote", "add", "origin", remoteURI}, projectDir, nil},
+		{[]string{"add", "."}, projectDir, nil},
+		{[]string{"commit", "-m", "test"}, projectDir,
+			[]string{
+				"GIT_COMMITTER_NAME=test",
+				"GIT_COMMITTER_EMAIL=test@example.com",
+				"GIT_AUTHOR_NAME=test",
+				"GIT_AUTHOR_EMAIL=test@example.com",
+			}},
+	}
+
+	// Setup a bare Git repository for pushing only
+	for _, command := range gitCommands {
+		gitCmd := exec.Command(gitExec, command.args...)
+		gitCmd.Dir = command.dir
+		if command.env != nil {
+			gitCmd.Env = append(os.Environ(), command.env...)
+		}
+
+		err := gitCmd.Run()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	// Cleanup
+	return func() {
+		os.RemoveAll(tempDir)
+		os.RemoveAll(filepath.Join(projectPath, ".git"))
 	}
 }
