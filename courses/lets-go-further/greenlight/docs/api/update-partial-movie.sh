24fba4d6c2d4850ef8af2884170cbdf4c3a3d64e
Alain D'Ettorre <alain.det@gmail.com>
2023-08-04 17:29:34 +0200

    Update /docs/api

--- courses/lets-go-further/greenlight/docs/api/update-partial-movie.sh
+++ courses/lets-go-further/greenlight/docs/api/update-partial-movie.sh
@@ -2,7 +2,7 @@ API_URL=http://localhost:4000/v1.0
 
 (
 curl \
---request PUT \
+--request PATCH \
 --url $API_URL/movies/2 \
 --header 'Content-Type: application/json; charset=utf-8' \
 --data-binary @- << EOF
