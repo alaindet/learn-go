0a86ba2ac9cad8893f5a34abbfaff0c67d400e38
Alain D'Ettorre <alain.det@gmail.com>
2023-08-14 11:34:25 +0200

    Fix duplicate email bug

--- courses/lets-go-further/greenlight/internal/data/users/errors.go
+++ courses/lets-go-further/greenlight/internal/data/users/errors.go
@@ -3,6 +3,11 @@ package users
 import "strings"
 
 func isDuplicateEmailErr(err error) bool {
+
+	if err == nil {
+		return false
+	}
+
 	duplicateErrMessage := "duplicate key value violates unique constraint"
 	return strings.Contains(err.Error(), duplicateErrMessage)
 }
