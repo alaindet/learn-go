f200be2e588025986c1d3abea95239627d2cc1f3
Alain D'Ettorre <alain.det@gmail.com>
2026-06-10 23:56:55 +0200

    feat: add boilerplate for broker service

---
+++ courses/go-microservices/broker/cmd/api/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "80"
+
+type Config struct {
+}
+
+func main() {
+	app := Config{}
+
+	log.Printf("Starting broker on port %s\n", webPort)
+
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Panic(err)
+	}
+}
---
+++ courses/go-microservices/broker/go.mod
@@ -0,0 +1,8 @@
+module broker
+
+go 1.25.0
+
+require (
+	github.com/go-chi/chi/v5 v5.3.0 // indirect
+	github.com/go-chi/cors v1.2.2 // indirect
+)
